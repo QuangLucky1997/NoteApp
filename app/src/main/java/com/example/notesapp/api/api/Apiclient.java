9e3fc885c1bf9a2725bf5ae3f7ae332b4e8d8e7b
devhue97 <namquangk39c@gmail.com>
2021-02-24 18:38:02 +0700

    update

--- app/src/main/java/com/example/notesapp/api/api/Apiclient.java
+++ app/src/main/java/com/example/notesapp/api/api/Apiclient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class Apiclient {
-    private static final String base_url = "http://192.168.143.2/NoteApp/";
+    private static final String base_url = "http://192.168.1.4/NoteApp/";
     private static Retrofit retrofit;
 
     public static Retrofit getApiClient() {
